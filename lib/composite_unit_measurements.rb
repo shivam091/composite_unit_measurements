e1323a70de0f624013dbdcb22087c1f9bb9de2bf
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-11-17 19:01:41 +0530

    Added ability to parse `pound-ounce` and `stone-pound` weight measurement

--- lib/composite_unit_measurements.rb
+++ lib/composite_unit_measurements.rb
@@ -6,3 +6,4 @@ require "composite_unit_measurements/base"
 
 require "composite_unit_measurements/length"
 require "composite_unit_measurements/time"
+require "composite_unit_measurements/weight"
---
+++ spec/composite_unit_measurements/weight_spec.rb
@@ -0,0 +1,50 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/composite_unit_measurements/weight_spec.rb
+
+RSpec.describe CompositeUnitMeasurements::Weight do
+  subject(:weight) { described_class }
+
+  describe "#parse" do
+    context "when valid string is passed" do
+      it "parses pound and ounce" do
+        expect(subject.parse("8 lb 12 oz").to_s).to eq("8.75 lb")
+        expect(subject.parse("8 lbs 12 ounce").to_s).to eq("8.75 lb")
+        expect(subject.parse("8 lbm 12 ounce").to_s).to eq("8.75 lb")
+        expect(subject.parse("8 # 12 ounce").to_s).to eq("8.75 lb")
+        expect(subject.parse("8 pound 12 ounce").to_s).to eq("8.75 lb")
+        expect(subject.parse("8 pounds 12 ounces").to_s).to eq("8.75 lb")
+        expect(subject.parse("8 pound-mass 12 oz").to_s).to eq("8.75 lb")
+      end
+
+      it "parses stone and pound" do
+        expect(subject.parse("2 st 6 lb").to_s).to eq("2.428571428571429 st")
+        expect(subject.parse("2 st 6 lbs").to_s).to eq("2.428571428571429 st")
+        expect(subject.parse("2 stone 6 lbm").to_s).to eq("2.428571428571429 st")
+        expect(subject.parse("2 stone 6 #").to_s).to eq("2.428571428571429 st")
+        expect(subject.parse("2 stones 6 pound").to_s).to eq("2.428571428571429 st")
+        expect(subject.parse("2 stones 6 pounds").to_s).to eq("2.428571428571429 st")
+      end
+    end
+
+    context "when invalid string is passed" do
+      it "raises an error" do
+        expect { subject.parse("8 lb 12 ozz") }.to raise_error(UnitMeasurements::ParseError)
+        expect { subject.parse("8 lbb 12 ozz") }.to raise_error(UnitMeasurements::ParseError)
+        expect { subject.parse("8 ## 12 ounces") }.to raise_error(UnitMeasurements::ParseError)
+        expect { subject.parse("8 lbms 12 oz") }.to raise_error(UnitMeasurements::ParseError)
+        expect { subject.parse("8 lbms 12 oz") }.to raise_error(UnitMeasurements::ParseError)
+        expect { subject.parse("8 poundss 12 ouncess") }.to raise_error(UnitMeasurements::ParseError)
+        expect { subject.parse("8 lbs 12 ounc") }.to raise_error(UnitMeasurements::ParseError)
+
+        expect { subject.parse("2 sts 6 lbs") }.to raise_error(UnitMeasurements::ParseError)
+        expect { subject.parse("2 stoness 6 poundss") }.to raise_error(UnitMeasurements::ParseError)
+        expect { subject.parse("2 st 6 pound mass") }.to raise_error(UnitMeasurements::ParseError)
+        expect { subject.parse("2 st 6 pound mass") }.to raise_error(UnitMeasurements::ParseError)
+        expect { subject.parse("2 st 6 ##") }.to raise_error(UnitMeasurements::ParseError)
+      end
+    end
+  end
+end
