fc2587b831ae5b423366429cda1a6514aefad540
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-11-28 11:02:20 +0530

    Removed unneeded `subject` in specs

--- spec/composite_unit_measurements/length_spec.rb
+++ spec/composite_unit_measurements/length_spec.rb
@@ -5,49 +5,47 @@
 # spec/composite_unit_measurements/length_spec.rb
 
 RSpec.describe CompositeUnitMeasurements::Length do
-  subject(:length) { described_class }
-
-  describe "#parse" do
+  describe ".parse" do
     context "when valid string is passed" do
       it "parses foot and inch" do
-        expect(length.parse("5 \' 6 \"").to_s).to eq("5.5 ft")
-        expect(length.parse("5 ft 6 in").to_s).to eq("5.5 ft")
-        expect(length.parse("5 foot 6 inch").to_s).to eq("5.5 ft")
-        expect(length.parse("5 feet 6 inches").to_s).to eq("5.5 ft")
+        expect(described_class.parse("5 \' 6 \"").to_s).to eq("5.5 ft")
+        expect(described_class.parse("5 ft 6 in").to_s).to eq("5.5 ft")
+        expect(described_class.parse("5 foot 6 inch").to_s).to eq("5.5 ft")
+        expect(described_class.parse("5 feet 6 inches").to_s).to eq("5.5 ft")
       end
 
       it "parses metre and centimetre" do
-        expect(length.parse("6 m 50 cm").to_s).to eq("6.5 m")
-        expect(length.parse("6 meter 50 centimeter").to_s).to eq("6.5 m")
-        expect(length.parse("6 metre 50 centimetre").to_s).to eq("6.5 m")
-        expect(length.parse("6 meters 50 centimeters").to_s).to eq("6.5 m")
-        expect(length.parse("6 metres 50 centimetres").to_s).to eq("6.5 m")
+        expect(described_class.parse("6 m 50 cm").to_s).to eq("6.5 m")
+        expect(described_class.parse("6 meter 50 centimeter").to_s).to eq("6.5 m")
+        expect(described_class.parse("6 metre 50 centimetre").to_s).to eq("6.5 m")
+        expect(described_class.parse("6 meters 50 centimeters").to_s).to eq("6.5 m")
+        expect(described_class.parse("6 metres 50 centimetres").to_s).to eq("6.5 m")
       end
 
       it "parses kilometre and metre" do
-        expect(length.parse("5 km 500 m").to_s).to eq("5.5 km")
-        expect(length.parse("5 kilometer 500 meter").to_s).to eq("5.5 km")
-        expect(length.parse("5 kilometers 500 meters").to_s).to eq("5.5 km")
-        expect(length.parse("5 kilometre 500 metre").to_s).to eq("5.5 km")
-        expect(length.parse("5 kilometres 500 metres").to_s).to eq("5.5 km")
+        expect(described_class.parse("5 km 500 m").to_s).to eq("5.5 km")
+        expect(described_class.parse("5 kilometer 500 meter").to_s).to eq("5.5 km")
+        expect(described_class.parse("5 kilometers 500 meters").to_s).to eq("5.5 km")
+        expect(described_class.parse("5 kilometre 500 metre").to_s).to eq("5.5 km")
+        expect(described_class.parse("5 kilometres 500 metres").to_s).to eq("5.5 km")
       end
     end
 
     context "when invalid string is passed" do
       it "raises an error" do
-        expect { length.parse("5 feets 6 inches") }.to raise_error(UnitMeasurements::ParseError)
-        expect { length.parse("5 feets 6 inche").to_s }.to raise_error(UnitMeasurements::ParseError)
-        expect { length.parse("5 foots 6 inched").to_s }.to raise_error(UnitMeasurements::ParseError)
-        expect { length.parse("5 fts 6 ines").to_s }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("5 feets 6 inches") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("5 feets 6 inche").to_s }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("5 foots 6 inched").to_s }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("5 fts 6 ines").to_s }.to raise_error(UnitMeasurements::ParseError)
 
-        expect { length.parse("6 ms 50 cms") }.to raise_error(UnitMeasurements::ParseError)
-        expect { length.parse("6 meterss 50 centimeterss") }.to raise_error(UnitMeasurements::ParseError)
-        expect { length.parse("6 metrez 50 centimetrez") }.to raise_error(UnitMeasurements::ParseError)
-        expect { length.parse("6 metrez 50 centimetrez") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("6 ms 50 cms") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("6 meterss 50 centimeterss") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("6 metrez 50 centimetrez") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("6 metrez 50 centimetrez") }.to raise_error(UnitMeasurements::ParseError)
 
-        expect { length.parse("5 kms 500 ms") }.to raise_error(UnitMeasurements::ParseError)
-        expect { length.parse("5 kilometerss 500 meterss") }.to raise_error(UnitMeasurements::ParseError)
-        expect { length.parse("5 kilometerz 500 meterz") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("5 kms 500 ms") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("5 kilometerss 500 meterss") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("5 kilometerz 500 meterz") }.to raise_error(UnitMeasurements::ParseError)
       end
     end
   end
--- spec/composite_unit_measurements/weight_spec.rb
+++ spec/composite_unit_measurements/weight_spec.rb
@@ -5,57 +5,55 @@
 # spec/composite_unit_measurements/weight_spec.rb
 
 RSpec.describe CompositeUnitMeasurements::Weight do
-  subject(:weight) { described_class }
-
-  describe "#parse" do
+  describe ".parse" do
     context "when valid string is passed" do
       it "parses pound and ounce" do
-        expect(subject.parse("8 lb 12 oz").to_s).to eq("8.75 lb")
-        expect(subject.parse("8 lbs 12 ounce").to_s).to eq("8.75 lb")
-        expect(subject.parse("8 lbm 12 ounce").to_s).to eq("8.75 lb")
-        expect(subject.parse("8 # 12 ounce").to_s).to eq("8.75 lb")
-        expect(subject.parse("8 pound 12 ounce").to_s).to eq("8.75 lb")
-        expect(subject.parse("8 pounds 12 ounces").to_s).to eq("8.75 lb")
-        expect(subject.parse("8 pound-mass 12 oz").to_s).to eq("8.75 lb")
+        expect(described_class.parse("8 lb 12 oz").to_s).to eq("8.75 lb")
+        expect(described_class.parse("8 lbs 12 ounce").to_s).to eq("8.75 lb")
+        expect(described_class.parse("8 lbm 12 ounce").to_s).to eq("8.75 lb")
+        expect(described_class.parse("8 # 12 ounce").to_s).to eq("8.75 lb")
+        expect(described_class.parse("8 pound 12 ounce").to_s).to eq("8.75 lb")
+        expect(described_class.parse("8 pounds 12 ounces").to_s).to eq("8.75 lb")
+        expect(described_class.parse("8 pound-mass 12 oz").to_s).to eq("8.75 lb")
       end
 
       it "parses stone and pound" do
-        expect(subject.parse("2 st 6 lb").to_s).to eq("2.428571428571429 st")
-        expect(subject.parse("2 st 6 lbs").to_s).to eq("2.428571428571429 st")
-        expect(subject.parse("2 stone 6 lbm").to_s).to eq("2.428571428571429 st")
-        expect(subject.parse("2 stone 6 #").to_s).to eq("2.428571428571429 st")
-        expect(subject.parse("2 stones 6 pound").to_s).to eq("2.428571428571429 st")
-        expect(subject.parse("2 stones 6 pounds").to_s).to eq("2.428571428571429 st")
+        expect(described_class.parse("2 st 6 lb").to_s).to eq("2.428571428571429 st")
+        expect(described_class.parse("2 st 6 lbs").to_s).to eq("2.428571428571429 st")
+        expect(described_class.parse("2 stone 6 lbm").to_s).to eq("2.428571428571429 st")
+        expect(described_class.parse("2 stone 6 #").to_s).to eq("2.428571428571429 st")
+        expect(described_class.parse("2 stones 6 pound").to_s).to eq("2.428571428571429 st")
+        expect(described_class.parse("2 stones 6 pounds").to_s).to eq("2.428571428571429 st")
       end
 
       it "parses kilogramme and gramme" do
-        expect(subject.parse("4 kg 500 g").to_s).to eq("4.5 kg")
-        expect(subject.parse("4 kilogramme 500 gramme").to_s).to eq("4.5 kg")
-        expect(subject.parse("4 kilogrammes 500 grammes").to_s).to eq("4.5 kg")
-        expect(subject.parse("4 kilogram 500 gram").to_s).to eq("4.5 kg")
-        expect(subject.parse("4 kilograms 500 grams").to_s).to eq("4.5 kg")
+        expect(described_class.parse("4 kg 500 g").to_s).to eq("4.5 kg")
+        expect(described_class.parse("4 kilogramme 500 gramme").to_s).to eq("4.5 kg")
+        expect(described_class.parse("4 kilogrammes 500 grammes").to_s).to eq("4.5 kg")
+        expect(described_class.parse("4 kilogram 500 gram").to_s).to eq("4.5 kg")
+        expect(described_class.parse("4 kilograms 500 grams").to_s).to eq("4.5 kg")
       end
     end
 
     context "when invalid string is passed" do
       it "raises an error" do
-        expect { subject.parse("8 lb 12 ozz") }.to raise_error(UnitMeasurements::ParseError)
-        expect { subject.parse("8 lbb 12 ozz") }.to raise_error(UnitMeasurements::ParseError)
-        expect { subject.parse("8 ## 12 ounces") }.to raise_error(UnitMeasurements::ParseError)
-        expect { subject.parse("8 lbms 12 oz") }.to raise_error(UnitMeasurements::ParseError)
-        expect { subject.parse("8 lbms 12 oz") }.to raise_error(UnitMeasurements::ParseError)
-        expect { subject.parse("8 poundss 12 ouncess") }.to raise_error(UnitMeasurements::ParseError)
-        expect { subject.parse("8 lbs 12 ounc") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("8 lb 12 ozz") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("8 lbb 12 ozz") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("8 ## 12 ounces") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("8 lbms 12 oz") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("8 lbms 12 oz") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("8 poundss 12 ouncess") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("8 lbs 12 ounc") }.to raise_error(UnitMeasurements::ParseError)
 
-        expect { subject.parse("2 sts 6 lbs") }.to raise_error(UnitMeasurements::ParseError)
-        expect { subject.parse("2 stoness 6 poundss") }.to raise_error(UnitMeasurements::ParseError)
-        expect { subject.parse("2 st 6 pound mass") }.to raise_error(UnitMeasurements::ParseError)
-        expect { subject.parse("2 st 6 pound mass") }.to raise_error(UnitMeasurements::ParseError)
-        expect { subject.parse("2 st 6 ##") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("2 sts 6 lbs") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("2 stoness 6 poundss") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("2 st 6 pound mass") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("2 st 6 pound mass") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("2 st 6 ##") }.to raise_error(UnitMeasurements::ParseError)
 
-        expect { subject.parse("4 kgs 500 gs") }.to raise_error(UnitMeasurements::ParseError)
-        expect { subject.parse("4 kilogramz 500 gramz") }.to raise_error(UnitMeasurements::ParseError)
-        expect { subject.parse("4 kilograms 500 gramss") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("4 kgs 500 gs") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("4 kilogramz 500 gramz") }.to raise_error(UnitMeasurements::ParseError)
+        expect { described_class.parse("4 kilograms 500 gramss") }.to raise_error(UnitMeasurements::ParseError)
       end
     end
   end
